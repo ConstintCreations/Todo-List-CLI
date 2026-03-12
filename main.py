f283517a9a31e0db7e0c8672de962fdc521daec8
ConstintCreations <ethanglenn1208@gmail.com>
2026-03-12 10:02:18 -0500

    Task Clearing and No Tasks Messages

--- main.py
+++ main.py
@@ -20,6 +20,7 @@ parser = argparse.ArgumentParser()
 parser.add_argument("-l", "--list", help="List all tasks", action="store_true")
 parser.add_argument("-c", "--complete", type=int, help="Mark a task as complete by ID")
 parser.add_argument("-d", "--delete", type=int, help="Delete a task by ID")
+parser.add_argument("-clr", "--clear", help="Clear all tasks", action="store_true")
 
 parser.add_argument("task", type=str, nargs="?", help="Task to add")
 
@@ -31,26 +32,39 @@ if len(sys.argv) == 1:
 
 if args.list:
     tasks = load_tasks()
-    for task in tasks:
-        status = "x" if task["done"] else " "
-        print(f"[{status}] {task['id']}: {task['task']}")
+    if len(tasks) == 0:
+        print("No tasks found")
+    else:
+        for task in tasks:
+            status = "x" if task["done"] else " "
+            print(f"[{status}] {task['id']}: {task['task']}")
     sys.exit(0)
 elif args.complete:
     tasks = load_tasks()
+    found_task = False
     for task in tasks:
         if task["id"] == args.complete:
+            found_task = True
             task["done"] = True
             save_tasks(tasks)
             print(f"Task {args.complete} marked as complete")
             break
+    if not found_task:
+        print(f"Task with ID of {args.complete} not found")
 elif args.delete:
     tasks = load_tasks()
-    new_tasks = []
+    found_task = False
     for task in tasks:
-        if task["id"] != args.delete:
-            new_tasks.append(task)
-    save_tasks(new_tasks)
-    print(f"Task with ID of {args.delete} deleted")
+        if task["id"] == args.delete:
+            found_task = True
+            tasks.remove(task)
+            save_tasks(tasks)
+            print(f"Task with ID of {args.delete} deleted")
+    if not found_task:
+        print(f"Task with ID of {args.delete} not found")
+elif args.clear:
+    save_tasks([])
+    print("All tasks have been cleared")
 elif args.task:
     tasks = load_tasks()
     if len(tasks) == 0:
